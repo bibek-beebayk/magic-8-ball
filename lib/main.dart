2249cbcabbf8eaa73a6f1c8f507f084dae1bfb9e
bibek-beebayk <beebayk0001@gmail.com>
2021-08-03 20:06:05 +0545

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,41 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text('Magic 8 ball'),
+            ),
+          ),
+          body: MyApp(),
+        ),
       ),
     );
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int ballNum = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: TextButton(
+          child: Image.asset('images/ball$ballNum.png'),
+          onPressed: () {
+            setState(() {
+              ballNum = Random().nextInt(5) + 1;
+              print(ballNum);
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
